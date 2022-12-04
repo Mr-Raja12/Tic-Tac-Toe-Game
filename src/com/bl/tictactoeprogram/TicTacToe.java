e00413ef2e8629a8e72ccc8df0380b016f035290
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-05 00:38:16 +0530

    Added: Allowing Player to Choose X or O

--- src/com/bl/tictactoeprogram/TicTacToe.java
+++ src/com/bl/tictactoeprogram/TicTacToe.java
@@ -1,41 +1,62 @@
 package com.bl.tictactoeprogram;
 import java.util.Scanner;
 /**
- * 
  * @author Raja
  *Starting is to show a Welcome message to this game.
  *Create the Tic Tac Toe board.
+ *Allow player to to chose X O
  */
 public class TicTacToe {
-	/**
-	 * Created a default constructor
-	 */
-	
+
+    //Creating a default constructor
     public TicTacToe() {
         System.out.println("Welcome to Tic Tac Toe Game !!!");
     }
+
     static char[] board = new char[10];
     static char player, computer;
-    Scanner scanner = new Scanner(System.in);
-    public void initialize() {
-        for (int i = 1; i < 10; i++) {
-            board[i] = ' ';
-        }
-    }
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        /**
-         * Initialize the object
-         */
+
+        //Initialize the object
         TicTacToe obj = new TicTacToe();
-        /**
-         * Creating the board
-         */
+
+        //Creating the board
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+
+        //Checking for player choice
+        chooseOption();
+
+    }
+
+    //Initialization of game
+    public static void initialize() {
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
+
+    //Allow player to choose X or O
+    public static void chooseOption()
+    {
+        System.out.println("Please Select Your Choice Letter : \nProvide 'X' or 'O'");
+        char choice = scan.next().charAt(0);
+        if (choice == 'X' || choice == 'x')
+        {
+            player = 'X';
+            computer = 'O';
+        }
+        else if (choice == 'O' || choice == 'o')
+        {
+            player = 'O';
+            computer = 'X';
+        }
+        System.out.println("Player choosing option : "+player);
     }
 }
-
 
