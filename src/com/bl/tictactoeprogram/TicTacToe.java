3aa44118b1eccbb10954edd1ce7e67c246dd7f00
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-07 08:10:20 +0530

    Added: To grab the corner position first during playing the game.

--- src/com/bl/tictactoeprogram/TicTacToe.java
+++ src/com/bl/tictactoeprogram/TicTacToe.java
@@ -1,4 +1,4 @@
-//Use Case 9 is to check block position and allow player to block the opponent.
+//Use Case 10 is to grab the corner position first during playing the game.
 
 package com.bl.tictactoeprogram;
 
@@ -7,50 +7,55 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-	// Creating a default constructor
-	public TicTacToe() {
-		System.out.println("Welcome to Tic Tac Toe Game !!!");
-	}
+    /*
+     * Creating a default constructor
+     */
+    public TicTacToe() {
+        System.out.println("Welcome to Tic Tac Toe Game !!!");
+    }
 
-	// Declaring variables
-	static char[] board = new char[10];// For board
-	static char player, computer;// For assigning x or o
-	static int playerLocation, computerLocation;// For player location
-	static int toss;// For tossing between player and computer
-	static boolean computerFlag = false, playerFlag = false;
-	static Scanner scan = new Scanner(System.in);
-	static Random random = new Random();
+    //Declaring variables
+    static char[] board = new char[10];//For board
+    static char player, computer;//For assigning x or o
+    static int playerLocation, computerLocation;//For player location
+    static int toss;//For tossing between player and computer
+    static boolean computerFlag = false, playerFlag = false;
+    static Scanner scan = new Scanner(System.in);
+    static Random random = new Random();
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Initialize the object
-		TicTacToe obj = new TicTacToe();
+        //Initialize the object
+        TicTacToe obj = new TicTacToe();
 
-		// Starting the game.
-		startGame();
+        //Starting the game.
+        startGame();
 
-	}
+    }
 
-	// Starts the game
-	public static void startGame() {
-		int freeSpace = 8;// after toss no of position remaining 8.
-		initialize();// Initialize the board
-		showBoard();// Showing the board
-		chooseOption();// Check for player option
-		toss();// Toss to check that who is going to play first
-		while (freeSpace != 0) {
-			System.out.println();
-			freeSpace--;
-			turn();
-			if (checkWinner()) {
-				break;
-			}
-		}
-		if (freeSpace == 0) {
-			System.out.println("Board is full");
-			System.out.println("It is a tie");
-		}
-	}
+    //Starts the game
+    public static void startGame() {
+        int freeSpace = 8;//after toss no of position remaining 8.
+        initialize();//Initialize the board
+        showBoard();//Showing the board
+        chooseOption();//Check for player option
+        toss();//Toss to check that who is going to play first
+        while (freeSpace != 0)
+        {
+            System.out.println();
+            freeSpace--;
+            turn();
+            if (checkWinner())
+            {
+                break;
+            }
+        }
+        if (freeSpace == 0)
+        {
+            System.out.println("Board is full");
+            System.out.println("It is a tie");
+        }
+    }
 
     public static boolean checkWinner()
     {
@@ -74,134 +79,197 @@ public class TicTacToe {
                 (board[1] == computer && board[4] == computer && board[7] == computer) ||
                 (board[2] == computer && board[5] == computer && board[8] == computer) ||
                 (board[3] == computer && board[6] == computer && board[9] == computer))
-		{
-			System.out.println("Wait we got winner\nWinner is Computer");
-			return true;
-		}
-		return false;
-	}
+        {
+            System.out.println("Wait we got winner\nWinner is Computer");
+            return true;
+        }
+        return false;
+    }
 
-	// Initialization of game
-	public static void initialize() {
-		for (int i = 1; i < 10; i++) {
-			board[i] = ' ';
-		}
-	}
+    //Initialization of game
+    public static void initialize()
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            board[i] = ' ';
+        }
+    }
 
-	// Doing a toss for playing first
-	public static void toss() {
-		toss = random.nextInt(2);
-		switch (toss) {
-		case 0:
-			System.out.println("Flipping Tail.\nComputer starts first.");
-			computerMove();// Computer move
-			computerFlag = true;
-			break;
-		case 1:
-			System.out.println("Flipping Head.\nPlayer starts first.");
-			playerMove();// Player move
-			playerFlag = true;
-			break;
-		}
-	}
+    //Doing a toss for playing first
+    public static void toss()
+    {
+        toss = random.nextInt(2);
+        switch (toss)
+        {
+            case 0 :
+                System.out.println("Flipping Tail.\nComputer starts first.");
+                computerMove();//Computer move
+                computerFlag = true;
+                break;
+            case 1 :
+                System.out.println("Flipping Head.\nPlayer starts first.");
+                playerMove();//Player move
+                playerFlag = true;
+                break;
+        }
+    }
 
-	// Allow player to choose X or O
-	public static void chooseOption() {
-		System.out.println("Please Select Your Choice Letter : \nProvide 'X' or 'O'");
-		player = check();
-		System.out.println("Player choosing option : " + player);
-	}
+    /*
+     * Allow player to choose X or O
+     */
+    public static void chooseOption() {
+        System.out.println("Please Select Your Choice Letter : \nProvide 'X' or 'O'");
+        player = check();
+        System.out.println("Player choosing option : "+player);
+    }
+    public static char check()//Sub method of chooseOption
+    {
+        char choice = scan.next().charAt(0);
+        if (choice == 'X' || choice == 'x')
+        {
+            player = 'X';
+            computer = 'O';
+        }
+        else if (choice == 'O' || choice == 'o')
+        {
+            player = 'O';
+            computer = 'X';
+        }
+        else
+        {
+            System.out.println("Invalid option.\nProvide the valid one");
+            check();
+        }
+        return player;
+    }
 
-	public static char check()// Sub method of chooseOption
-	{
-		char choice = scan.next().charAt(0);
-		if (choice == 'X' || choice == 'x') {
-			player = 'X';
-			computer = 'O';
-		} else if (choice == 'O' || choice == 'o') {
-			player = 'O';
-			computer = 'X';
-		} else {
-			System.out.println("Invalid option.\nProvide the valid one");
-			check();
-		}
-		return player;
-	}
+    //To see board
+    public static void showBoard()
+    {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("---------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("---------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
 
-	// To see board
-	public static void showBoard() {
-		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
-		System.out.println("---------");
-		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-		System.out.println("---------");
-		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-	}
+    //To make the player move
+    public static void playerMove()
+    {
+        checkFreeSpace();
+        int position = blockUser(computer);
+        essentialPosition();
+        if (position == 0)
+        {
+            System.out.println("No position to be blocked.\nTry to win");
+        }
+        else if (position != 0)
+        {
+            System.out.println("Block you opponent on position : "+position);
+            System.out.println("If you don't want to block try to win");
+        }
+        System.out.println("Enter the position between (1-9) you want to make your move :");
+        playerLocation = scan.nextInt();
+        if (playerLocation > 0 && playerLocation < 10)
+        {
+            if (board[playerLocation] == ' ')
+            {
+                board[playerLocation] = player;
+                showBoard();
+            }
+            else if (board[playerLocation] != ' ')
+            {
+                System.err.println("Ah-hh! Position is already chosen. Enter a valid position");
+                showBoard();
+                playerMove();
+                showBoard();
+            }
+        }
+        else
+        {
+            System.err.println("Invalid choice. Provide a valid position between (1-9)");
+            playerMove();
+        }
+    }
 
-	// To make the player move
-	public static void playerMove() {
-		checkFreeSpace();
-		int position = blockUser(computer);
-		if (position == 0) {
-			System.out.println("No position to be blocked.\nTry to win");
-		} else if (position != 0) {
-			System.out.println("Block you opponent on position : " + position);
-			System.out.println("If you don't want to block try to win");
-		}
-		System.out.println("Enter the position between (1-9) you want to make your move :");
-		playerLocation = scan.nextInt();
-		if (playerLocation > 0 && playerLocation < 10) {
-			if (board[playerLocation] == ' ') {
-				board[playerLocation] = player;
-				showBoard();
-			} else if (board[playerLocation] != ' ') {
-				System.err.println("Ah-hh! Position is already chosen. Enter a valid position");
-				showBoard();
-				playerMove();
-				showBoard();
-			}
-		} else {
-			System.err.println("Invalid choice. Provide a valid position between (1-9)");
-			playerMove();
-		}
-	}
+    /*
+     * Check for essential position
+     */
+    public static void essentialPosition() {
+        for (int i = 1; i < board.length; i++)
+        {
+            if ( i == 1 && board[i] == ' ')
+            {
+                System.out.println("Corner position 1 is available");
+            }
+            else if (i == 3 && board[i] == ' ')
+            {
+                System.out.println("Corner position 3 is available");
+            }
+            else if (i == 7 && board[i] == ' ')
+            {
+                System.out.println("Corner position 7 is available");
+            }
+            else if (i == 9 && board[i] == ' ')
+            {
+                System.out.println("Corner position 9 is available");
+            }
+        }
+    }
 
-	// To make the computer move
-	public static void computerMove() {
-		int position = blockUser(player);
-		if (position == 0) {
-			computerLocation = random.nextInt(8) + 1;// Random gives 0 to 8 so +1 give you 1 to 9 position.
-		} else if (position != 0) {
-			System.out.println("Computer block the position : " + position);
-			computerLocation = position;
-		}
-		if (computerLocation > 0 && computerLocation < 10) {
-			if (board[computerLocation] == ' ') {
-				board[computerLocation] = computer;
-				showBoard();
-			} else if (board[computerLocation] != ' ') {
-				computerMove();
-			}
-		}
-	}
+    //To make the computer move
+    public static void computerMove()
+    {
+        int position = blockUser(player);
+        if (position == 0)
+        {
+            computerLocation = random.nextInt(8)+1;//Random gives 0 to 8 so +1 give you 1 to 9 position.
+        }
+        else if (position != 0)
+        {
+            System.out.println("Computer block the position : "+position);
+            computerLocation = position;
+        }
+        if (computerLocation > 0 && computerLocation < 10)
+        {
+            if (board[computerLocation] == ' ')
+            {
+                board[computerLocation] = computer;
+                showBoard();
+            }
+            else if (board[computerLocation] != ' ')
+            {
+                computerMove();
+            }
+        }
+    }
 
-	// To check free space
-	public static void checkFreeSpace() {
-		boolean isSpaceAvailable = false;
-		int numOfFreeSpaces = 0;
-		for (int index = 1; index < board.length; index++) {
-			if ((board[index] == ' ')) {
-				isSpaceAvailable = true;
-				numOfFreeSpaces++;
-			}
-		}
-		if (isSpaceAvailable == false) {
-			System.err.println("Board is full! You can't make another move");
-		} else {
-			System.out.println("Free space is available! you have " + numOfFreeSpaces + " moves left");
-		}
-	}
+    //To check free space
+    public static void checkFreeSpace()
+    {
+        boolean isSpaceAvailable = false;
+        int numOfFreeSpaces = 0;
+        for(int index=1; index<board.length; index++)
+        {
+            if((board[index] == ' '))
+            {
+                isSpaceAvailable = true;
+                numOfFreeSpaces++;
+            }
+        }
+        if(isSpaceAvailable == false)
+        {
+            System.err.println("Board is full! You can't make another move");
+        }
+        else
+        {
+            System.out.println("Free space is available! you have "+numOfFreeSpaces+ " moves left");
+        }
+    }
 
-    //Check for winning, tie or change turn
+    /*
+     * Check for winning, tie or change turn
+     */
     public static void checkGame()
     {
         if
